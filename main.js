c820979f76869fee90815186b2b9f19401d8ddf5
ranaiscool <116927234+ranaiscool@users.noreply.github.com>
2023-06-03 10:56:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+
+function set(){
+    canvas = createCanvas(500,500);
+    canvas.center();
+}
+function draw(){
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
